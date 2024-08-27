2d173ef6f28681aa2d3c9169a36a38f41258b739
Claire Lafitte <clairelafitte37@gmail.com>
2024-08-27 16:29:44 +0200

    Add login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ use App\Repository\ActivityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER', message: 'More priviledge are required to access the resource.')]
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
